8d953d933b8f37cc0639c724f622d5015e09c2c0
Thabang Teddy <rttteddy@gmail.com>
2025-05-27 09:00:13 +0200

    removed the bash and added output dir

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Use Python from the environment that Vercel provides
 python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput --clear
