6a9528014c36ac09df253da88ae92f8b203b0f73
Thabang Teddy <rttteddy@gmail.com>
2025-05-27 09:23:18 +0200

    removed the bash and added output dir

--- Web_Portfolio/settings.py
+++ Web_Portfolio/settings.py
@@ -130,7 +130,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    '/var/www/static/',
 ]
 
 # This is for Vercel deployment
