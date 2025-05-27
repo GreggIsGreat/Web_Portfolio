231ac9a960929370647325fa950975ce505dd41d
Thabang Teddy <rttteddy@gmail.com>
2025-05-27 10:52:48 +0200

    removed

--- Web_Portfolio/settings.py
+++ Web_Portfolio/settings.py
@@ -135,10 +135,11 @@ STATICFILES_DIRS = [
 
 # This is for Vercel deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+# Media files
+MEDIA_URL = '/media/'  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 
 
 # Default primary key field type
